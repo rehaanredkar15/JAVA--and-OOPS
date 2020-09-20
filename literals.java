74b83c890c66039c4401e1ba2ada715ab71351b0
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2020-09-20 21:02:05 +0530

    Add files via upload

---
+++ literals.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class literals {   //this is a class file
+    public static void main(String[] args) {     //just type main for this in intelij idea
+        byte age = 34;
+        int age2 = 56;
+        short age3 = 87;
+        long age4 = 56666666666l;  //U need  to add l at the end of the no to tell that it is not integer
+        char ch = 'A';
+        float f1 =  5.6f;  //by default it is double so we add f at the end
+        double d1 = 4.66;
+        boolean a = true;
+        System.out.println(age); //use sout as a short form
+        String str = "Rehaan";
+        System.out.println(str);
+
+    }
+}
