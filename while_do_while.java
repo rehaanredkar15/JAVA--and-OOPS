48d9d656208601c90263becc529cddf6673a1d6d
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2020-09-29 17:03:43 +0530

    Add files via upload

---
+++ while_do_while.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class while_do_while
+{
+    public static void main(String[] args)
+    {
+        System.out.println("THE WHILE LOOP");
+        int i = 0;
+        while(i<=10)
+        {
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("do while loop starts here ");
+        i=1;
+        do {
+            System.out.println(i);
+            i++;
+        }while(i<=30);
+    }
+
+}
