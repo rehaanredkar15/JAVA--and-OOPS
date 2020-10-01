9ecf8146d62d13083477f1065a02f7479e6782e2
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2020-10-01 21:24:05 +0530

    Add files via upload

---
+++ methods.java
@@ -0,0 +1,18 @@
+package com.company;
+
+
+public class methods {
+    public static void main(String[] args) {
+        System.out.println("methods");      /*here we have written the method ahe main function in same class
+                                              in same class we can create any amount of methods we want */
+        int y = mymethod(7);
+        System.out.println(y);
+    }
+
+    public static int mymethod(int s)   /* methods can also be called as functions as it works similarly to
+                                           functions , here we can pass parameters also */
+    {
+        System.out.println("hi");
+         s = 9;
+        return s;
+    }
