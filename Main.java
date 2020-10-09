58f8e90c6019b5e316bbee6c0455492ebb002848
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2020-10-09 18:45:29 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,19 +1,17 @@
-package com.company;
+package com.company;// the package inside which our program is
 
-public class Main {
+public class Main {  //the class which has main function in it so it has name similar to main func
 
-    public static void main(String[] args) {
-        teacher obj = new teacher();  //here the object of person is created and functions of person is called
-        obj.teach();
-        obj.name = "Rehan";
-        obj.eat();
-        obj.walk();
-        singer o =  new singer(); //here we can inherit the  function of person class in singer and teacher class
-        o.name = "Kōsei Arima";
-        o.sing();
-        o.eat(); // this function is overriding the function of parent class.
-
-
-
+    public static void main(String[] args) {  /* the main function and here the
+                                    static word is also used so that we can run the class without object s in c++
+                                    (static makes the function independent), void main(no return)
+                                    and the string args is used cause of the arguments which the function will
+                                     get inside */
+        System.out.print("The value of additions of the numbers will be : ");
+        int num1 = 1;
+        int num2 = 2;
+        int num3 = 3;
+        System.out.println(num1 + num2 + num3);/*remember here first the matter is printed and then the ln (next line)
+         is added */
     }
-} 
+}
